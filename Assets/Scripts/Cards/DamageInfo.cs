5959ec3ce604298c0bb77be50feae7ba26454aca
marssuren <287815741@qq.com>
2018-07-29 13:06:50 +0800

    提交代码

--- Assets/Scripts/Cards/DamageInfo.cs
+++ Assets/Scripts/Cards/DamageInfo.cs
@@ -35,6 +35,62 @@ public class DamageInfo : MonoBehaviour
 		}
 	}
 
+	public void ApplyEnemyPowersOnly(AbstractCreature _target)
+	{
+		Output = Base;
+		IsModified = false;
+		float tValue = Output;
+		AbstractPower tPower;
+		for (int i = 0; i < _target.powers.Count; i++)
+		{
+			tPower = _target.powers[i];
+			tValue = tPower.AtDamageReceive(Output, Type);
+			if (Base!=Output)
+			{
+				IsModified = true;
+			}
+		}
+
+		for (int i = 0; i < _target.powers.Count; i++)
+		{
+			tPower = _target.powers[i];
+			tValue = tPower.AtDamageFinalReceive(Output, Type);
+			if (Base!=Output)
+			{
+				IsModified = true;
+			}
+		}
+
+		if (tValue<0f)
+		{
+			tValue = 0f;
+		}
+
+		Output = (int)Mathf.Floor(tValue);
+	}
+
+	public static int[] CreateDamageMatrix(int _baseDamage)
+	{
+		return CreateDamageMatrix(_baseDamage, false);
+	}
+
+	public static int[] CreateDamageMatrix(int _baseDamage, bool _isPureDamage)
+	{
+		int[] tRetVal=new int[AbstractDungeon.GetMonsters().Monsters.Count];
+		for (int i = 0; i < tRetVal.Length; i++)
+		{
+			DamageInfo tDamageInfo=new DamageInfo(AbstractDungeon.Player,_baseDamage);
+			if (!_isPureDamage)
+			{
+				tDamageInfo.ApplyEnemyPowersOnly(AbstractDungeon.GetMonsters().Monsters[i]);
+			}
+
+			tRetVal[i] = tDamageInfo.Output;
+
+		}
+
+		return tRetVal;
+	}
 }
 public enum DamageType
 {
